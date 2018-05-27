44dc8936b751340b8c58bc80b54807a7d377c803
anikethsaha <anik220798@gmail.com>
2018-05-28 00:34:52 +0530

    Add files via upload

---
+++ protection_tokens/csrf_token.php
@@ -0,0 +1,25 @@
+<?php 
+
+
+// for csrf attack protection I have created this csrf token manually as I am not using any framework 
+// but if using framework I would have used there prebuilt token --  --by Aniketh
+ session_start();
+ session_regenerate_id();
+ $options = [
+    	'cost' => 10 
+    // 'salt' => base64_encode("anyEncryptedRandomString__") // I am using php 7.1.0 where salt option is removed
+];
+ 
+ $token =  password_hash(session_id(), PASSWORD_BCRYPT ,$options);
+ 
+
+ $cookie_name = "CSRF_TOKEN_ANIKETH";
+ $expire = time() + (86400 * 30);
+ $path = "/";
+ $domain = "";
+ $secure = false;
+ $httponly = true;
+
+ setcookie($cookie_name, $token, $expire, $path, $domain, $secure, $httponly );
+
+ ?>
---
+++ protection_tokens/session_encryption.php
@@ -0,0 +1,51 @@
+<?php 
+	/**
+	* 
+	*/
+	class session_Ecnryption{
+
+		protected $encrypt_method;
+		protected $secret_key;
+		protected $secret_iv;
+		protected $output;
+		protected $key;
+		protected $iv;
+		
+		
+		function __construct()
+		{
+			
+			$this->output = false;
+
+
+			$this->encrypt_method = "AES-256-CBC";
+		    $this->secret_key = 'Your_Secret_key_here';
+		    $this->secret_iv = 'Your_iv_here';
+
+		    // hash
+		    $this->key = hash('sha256', $this->secret_key);
+		    
+		    // iv - encrypt method AES-256-CBC expects 16 bytes - 
+		    $this->iv = substr(hash('sha256', $this->secret_iv), 0, 16);
+		}
+
+		public function Encrypt($string)
+		{
+			
+		   
+		    $this->output = openssl_encrypt($string, $this->encrypt_method, $this->key, 0, $this->iv);
+			return $this->output;	
+		}
+
+		public function Decrypt($string)
+		{
+			 $this->output = openssl_decrypt($string, $this->encrypt_method, $this->key, 0, $this->iv);
+			 return $this->output;
+		}
+
+
+	}
+	
+
+
+ ?>
